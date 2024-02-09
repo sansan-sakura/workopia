e931307ab0a5ebda6494c4fb17b75db519fdda3f
Sakura Tanaka <sito6496t@gmail.com>
2024-02-09 15:45:54 +0100

    refactore paths

--- helpers.php
+++ helpers.php
@@ -10,3 +10,39 @@
  function basePath($path=""){
     return __DIR__ . '/' . $path;
  }
+
+
+ /**
+  * load a view
+  * @param string $name
+  * @return void
+  */
+
+  function loadView($name){
+
+  $viewPath=basePath("views/{$name}.view.php");
+  if(file_exists($viewPath)){
+    require $viewPath;
+  }  else {
+    echo "View {$viewPath} is not found";
+  }
+  }
+
+
+ /**
+  * load a partials
+  * @param string $name
+  * @return void
+  */
+
+  function loadPartial($name)
+  {
+
+    $partialPath=basePath("views/partials/{$name}.php");
+
+    if(file_exists($partialPath)){
+        require $partialPath;
+    } else {
+        echo "Partial {$partialPath} is not found";
+    }
+  }
